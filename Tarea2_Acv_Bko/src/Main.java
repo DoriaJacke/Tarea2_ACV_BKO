6c3550998e7e6ef1406ab69f449669df526034aa
DoriaJacke <acv1324@gmail.com>
2025-06-02 13:40:23 -0400

    Add files via upload

---
+++ Tarea2_Acv_Bko/src/Main.java
@@ -0,0 +1,14 @@
+import Tarea2.backend.Evaluacion;
+import Tarea2.frontend.evaluacionGUI;
+
+public class Main {
+    public static void main(String[] args) {
+
+        // ** Backend
+        Evaluacion miEvaluacion = new Evaluacion();
+
+        // ** Frontend
+        evaluacionGUI frontend = new evaluacionGUI(miEvaluacion);
+        frontend.setVisible(true);
+    }
+}
